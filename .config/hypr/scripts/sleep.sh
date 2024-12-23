82d442db2c7b64d590fedf6ac046015a28c6d9aa
stggn <36167944+stggn@users.noreply.github.com>
2024-12-23 19:09:51 +0000

    Delete .config/hypr/scripts/sleep.sh

--- .config/hypr/scripts/sleep.sh
+++
@@ -1,6 +0,0 @@
-swayidle -w timeout 300 'swaylock -f -c 000000' \
-            timeout 600 'hyprctl dispatch dpms off; brightnessctl -d *::kbd_backlight -s s 0' \
-            resume 'hyprctl dispatch dpms on; brightnessctl -d *::kbd_backlight -r' \
-            timeout 900 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 000000' &
-
